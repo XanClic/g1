5eaf2ab43eba9f1c643cee0c6e91035b709f7831
Hanna Reitz <xanclic@xanclic.moe>
2014-08-30 03:25:52 +0200

    Use array textures
    + If there is no bindless texture support, use array textures instead

--- assets/cloud_nbl_frag.glsl
+++ assets/cloud_nbl_frag.glsl
@@ -3,17 +3,21 @@
 
 in vec3 vf_nrm;
 in vec2 vf_txc;
+flat in ivec2 vf_tex;
 
 out vec4 out_col;
 
-uniform sampler2D cloud_texture;
+uniform sampler2DArray cloud_texture;
+uniform vec4 cloud_texture_params[20];
 
 uniform vec3 light_dir;
 
 
 void main(void)
 {
+    vec2 ctxc = (vf_txc - cloud_texture_params[vf_tex.x].pq) * cloud_texture_params[vf_tex.x].st;
+
     float diff_co = max(0.0, dot(normalize(vf_nrm), -light_dir));
 
-    out_col = vec4(vec3(diff_co), texture(cloud_texture, vf_txc).r);
+    out_col = vec4(vec3(diff_co), texture(cloud_texture, vec3(ctxc, float(vf_tex.x))).r);
 }
