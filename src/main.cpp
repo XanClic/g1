d8194a07183e5a3388a805149c3e0d4fd58fd588
Max Reitz <xanclic@gmail.com>
2014-08-31 04:03:41 +0200

    Set options through command line
    + Add command line options for min and max LOD

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <cerrno>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <getopt.h>
+
 #include "main_loop.hpp"
 #include "options.hpp"
 #include "ui.hpp"
@@ -6,8 +16,63 @@
 Options global_options;
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    for (;;) {
+        static const struct option options[] = {
+            {"help", no_argument, nullptr, 'h'},
+            {"min-lod", required_argument, nullptr, 256},
+            {"max-lod", required_argument, nullptr, 257},
+        };
+
+        int option = getopt_long(argc, argv, "h", options, nullptr);
+        if (option == -1) {
+            break;
+        }
+
+        switch (option) {
+            case 'h':
+            case '?':
+                fprintf(stderr, "Usage: %s [options...]\n\n", argv[0]);
+                fprintf(stderr, "Options:\n");
+                fprintf(stderr, "  -h, --help       Shows this information\n");
+                fprintf(stderr, "  --min-lod=LOD    Sets the minimum LOD (0..8; default: 0)\n");
+                fprintf(stderr, "  --max-lod=LOD    Sets the maximum LOD (3..8; default: 8)\n");
+                return 0;
+
+            case 256: {
+                char *endp;
+                errno = 0;
+                unsigned long min_lod = strtoul(optarg, &endp, 0);
+                if (errno || (min_lod > 8) || *endp) {
+                    fprintf(stderr, "Invalid argument given for --min-lod\n");
+                    return 1;
+                }
+
+                global_options.min_lod = min_lod;
+                break;
+            }
+
+            case 257: {
+                char *endp;
+                errno = 0;
+                unsigned long max_lod = strtoul(optarg, &endp, 0);
+                if (errno || (max_lod < 3) || (max_lod > 8) || *endp) {
+                    fprintf(stderr, "Invalid argument given for --max-lod\n");
+                    return 1;
+                }
+
+                global_options.max_lod = max_lod;
+                break;
+            }
+        }
+    }
+
+    if (global_options.min_lod > global_options.max_lod) {
+        fprintf(stderr, "min-lod may not exceed max-lod\n");
+        return 1;
+    }
+
     init_ui();
 
     main_loop();
