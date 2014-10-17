d07011b31477cadd445d5d8a263360dbdcb1e4b9
Hanna Reitz <xanclic@xanclic.moe>
2014-10-17 18:52:18 +0200

    fetch-assets.rb: Parallel download
    + Download in parallel for greater speed (each single file is not that
      big)

--- tools/fetch-assets.rb
+++ tools/fetch-assets.rb
@@ -4,6 +4,44 @@
 require 'json'
 require 'net/http'
 
+
+class Enumerator
+    def par(thr)
+        threads = []
+        results = []
+
+        main_thread = Thread.current
+        j = 0
+
+        while true
+            while threads.size >= thr
+                (0..thr-1).each do |i|
+                    next if !threads[i] || threads[i][0].alive?
+                    results[threads[i][1]] = threads[i][0].value
+                    threads.delete_at(i)
+                end
+
+                Thread.stop if threads.size >= thr
+            end
+
+            begin
+                threads << [Thread.new(self.next) { |v| v = yield v; main_thread.wakeup; v }, j]
+            rescue StopIteration
+                break
+            end
+
+            j += 1
+        end
+
+        threads.each do |t|
+            results[t[1]] = t[0].value
+        end
+
+        return results
+    end
+end
+
+
 min_lod = 0
 target  = '../assets'
 
@@ -21,21 +59,24 @@ files = ([''] + min_lod.upto(8).map { |i| i.to_s }).map { |lod| object[lod] }.in
 
 i = 0
 fll = files.length.to_s.length
+mtx = Mutex.new
 
-files.each do |e, md5|
-    # Should be at the end, but well
-    i += 1
+files.each.par(16) do |e, md5|
+    mtx.synchronize do
+        perc = 100.0 * i / files.length
+        fw = 70.0 - 2 * fll
+        bw = (fw * i / files.length).round
+        ew = (fw - bw).round
 
-    perc = 100.0 * i / files.length
-    fw = 70.0 - 2 * fll
-    bw = (fw * i / files.length).round
-    ew = (fw - bw).round
-
-    print "%*i/#{files.length} %3i%% [#{'=' * bw}#{' ' * ew}]\r" % [fll, i, 100.0 * i / files.length]
-    $stdout.flush
+        print "%*i/#{files.length} %3i%% [#{'=' * bw}#{' ' * ew}]\r" % [fll, i, 100.0 * i / files.length]
+        $stdout.flush
+    end
 
     if File.file?("#{target}/#{e}")
         if `md5sum -b "#{target}/#{e}"`.split[0] == md5
+            mtx.synchronize do
+                i += 1
+            end
             next
         end
     end
@@ -44,6 +85,10 @@ files.each do |e, md5|
     system("mkdir -p '#{dn}'") unless File.directory?(dn)
 
     system("wget 'https://xanclic.moe/g1/#{e}' -O '#{target}/#{e}' 2>/dev/null")
+
+    mtx.synchronize do
+        i += 1
+    end
 end
 
 puts "#{files.length}/#{files.length} 100% [#{'=' * (70 - 2 * fll)}]"
