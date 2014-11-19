b64880a1ac18e0216ab82c4de5636227fc01d29b
Max Reitz <xanclic@gmail.com>
2014-11-19 17:01:19 +0100

    Use atmosphere color lookup texture
    + Including special night colors

--- shaders/atmof_frag.glsl
+++ shaders/atmof_frag.glsl
@@ -5,7 +5,7 @@ in vec3 vf_pos, vf_nrm;
 
 out vec4 out_col;
 
-uniform sampler2D color, depth, stencil;
+uniform sampler2D color, depth, stencil, atmo_map;
 uniform vec2 sa_z_dim, screen_dim;
 uniform vec3 cam_pos, cam_fwd, light_dir;
 
@@ -21,7 +21,7 @@ void main(void)
     vec3 old_sample = texture(color, screen_pos).rgb;
     float stencil_val = texture(stencil, screen_pos).r;
 
-    float div = 1000 + (1.0 - stencil_val) * (900.0 * (1.0 - length(gl_FragCoord.xy - 0.5 * screen_dim) / length(screen_dim)));
+    float div = 1000 + (1.0 - stencil_val) * (990.0 * (1.0 - length(gl_FragCoord.xy - 0.5 * screen_dim) / length(screen_dim)));
     float exp = 3.0 - 2.7 * stencil_val;
 
     float strength = pow(min(1.0, max((zb_a - zf_a), 0.0) / div), exp);
@@ -34,9 +34,10 @@ void main(void)
     float diffuse = dot(normal, -light_dir);
     float brightness = clamp(smoothstep(-0.3, 0.3, diffuse) * 1.5 + (to_viewer_strength - 1.0) * 0.5, 0.03, 1.0);
 
-    vec3 color = mix(vec3(0.51, 0.73, 1.0), vec3(1.0, 0.3, 0.0),
+    vec4 blue = texture(atmo_map, vec2(strength, stencil_val + 0.5 - 0.5 * brightness));
+    vec3 color = mix(blue.rgb, vec3(1.0, 0.3, 0.0),
                      smoothstep(-0.2, -0.1, diffuse) * pow(to_viewer_strength, 5.0));
 
     // Use alpha blending for background
-    out_col = vec4(mix(old_sample, brightness * color, min(strength + 1.0 - stencil_val, 1.0)), min(stencil_val + strength, 1.0));
+    out_col = vec4(mix(old_sample, brightness * color, blue.a), min(stencil_val + blue.a, 1.0));
 }
