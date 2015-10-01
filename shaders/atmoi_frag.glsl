44fb508a700e9819b6033b17e1b21f91c02c82ed
Hanna Reitz <xanclic@xanclic.moe>
2015-10-01 02:59:48 +0200

    Atmospheric shader: Make dawn/dusk nicer
    * ...by stretching the brightness over a larger area when close to the
      ground.

--- shaders/atmoi_frag.glsl
+++ shaders/atmoi_frag.glsl
@@ -32,14 +32,20 @@ void main(void)
     // FIXME (works fine as long as the earth is centered at (0, 0, 0), though)
     vec3 normal = normalize(cam_pos);
 
-    float to_viewer_strength = pow(clamp(dot(light_dir, to_viewer), 0.0, 1.0), 5.0);
+    float to_viewer_strength = mix(pow(0.5 * (dot(light_dir, to_viewer) + 1.0), 0.5),
+                                   pow(clamp(dot(light_dir, to_viewer), 0.0, 1.0), 5.0),
+                                   height);
 
     float diffuse = dot(normal, -light_dir);
     float brightness = clamp(smoothstep(-0.3, 0.3, diffuse) * 1.5 + (to_viewer_strength - 1.0) * 0.5, 0.03, 1.0);
 
+    float redness = mix(smoothstep(0.2, 0.0, abs(diffuse)),
+                        smoothstep(-0.2, -0.1, diffuse),
+                        height);
+
     vec4 blue = texture(atmo_map, vec2(strength, stencil_val + 0.5 - 0.5 * brightness));
     vec3 color = mix(blue.rgb, vec3(1.0, 0.3, 0.0),
-                     smoothstep(-0.2, -0.1, diffuse) * pow(to_viewer_strength, 5.0));
+                     redness * pow(to_viewer_strength, 5.0));
 
     // Use alpha blending for background
     out_col = vec4(mix(old_sample, brightness * color, blue.a), min(stencil_val + blue.a, 1.0));
