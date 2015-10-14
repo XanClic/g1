ba74c3bfcb4a0777505d6fb2ffcc1383b01a9d3c
Hanna Reitz <xanclic@xanclic.moe>
2015-10-14 22:54:31 +0200

    Particles: Elongate
    + Add a tail to each particle which makes them look more like what we
      know from certain other games *cough cough*

--- shaders/particle_geom.glsl
+++ shaders/particle_geom.glsl
@@ -2,8 +2,10 @@
 
 
 layout(points) in;
-layout(triangle_strip, max_vertices=4) out;
+layout(triangle_strip, max_vertices=8) out;
 
+
+in vec4 vg_start[];
 
 out vec2 gf_pos;
 
@@ -29,6 +31,33 @@ void main(void)
 
     gf_pos = vec2( 1.0,  1.0);
     gl_Position = gl_in[0].gl_Position + vec4(gf_pos * size, 0.0, 0.0);
+    EmitVertex();
+
+    EndPrimitive();
+
+
+    // TODO: Is this worth it?
+    if (length(gl_in[0].gl_Position.xy / gl_in[0].gl_Position.w -
+        vg_start[0].xy / vg_start[0].w) < 0.01)
+    {
+        return;
+    }
+
+
+    gf_pos = vec2(-1.0, 1.0);
+    gl_Position = vg_start[0] + vec4(-0.5, 0.0, 0.0, 0.0);
+    EmitVertex();
+
+    gf_pos = vec2( 1.0, 1.0);
+    gl_Position = vg_start[0] + vec4( 0.5, 0.0, 0.0, 0.0);
+    EmitVertex();
+
+    gf_pos = vec2(-1.0, 0.0);
+    gl_Position = gl_in[0].gl_Position + vec4(-0.5, 0.0, 0.0, 0.0);
+    EmitVertex();
+
+    gf_pos = vec2( 1.0, 0.0);
+    gl_Position = gl_in[0].gl_Position + vec4( 0.5, 0.0, 0.0, 0.0);
     EmitVertex();
 
     EndPrimitive();
--- shaders/particle_vert.glsl
+++ shaders/particle_vert.glsl
@@ -2,12 +2,16 @@
 
 
 in vec3 va_position;
-in vec3 va_velocity;
+in vec3 va_orientation;
+
+out vec4 vg_start;
 
 uniform mat4 mat_mvp;
 
 
 void main(void)
 {
+    vg_start = mat_mvp *
+               vec4(va_position - va_orientation, 1.0);
     gl_Position = mat_mvp * vec4(va_position, 1.0);
 }
