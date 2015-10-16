6d685f9a8ed9fb44ce27824a4a2ac4c9c382a364
Max Reitz <xanclic@gmail.com>
2015-10-16 05:23:41 +0200

    tools/index-assets.rb: Index everything in assets/
    * Now that assets/ should be clean, the script can simply index every
      file there is.

--- tools/index-assets.rb
+++ tools/index-assets.rb
@@ -2,22 +2,32 @@
 # coding: utf-8
 
 require 'json'
+require 'shellwords'
 
 if ARGV.length != 1
     $stderr.puts("Usage: ./index-assets.rb <assets directory>")
     exit 1
 end
 
+LOD_DIRS = ['earth', 'night', 'clouds']
+
 object = Hash.new
 
 object[nil] = Hash.new
-Dir.entries(ARGV[0]).select { |e| ['.jpg', '.png', '.xsmd'].find { |ext| e.end_with?(ext) } }.each do |e|
-    object[nil][e] = `md5sum -b "#{ARGV[0]}/#{e}"`.split[0]
+
+all_files = `find #{ARGV[0].shellescape} -type f -print0`.split("\0").map { |f|
+    f.sub(ARGV[0] + '/', '')
+}.reject { |f|
+    LOD_DIRS.find { |d| f.start_with?(d + '/') }
+}
+
+all_files.each do |f|
+    object[nil][f] = `md5sum -b "#{ARGV[0]}/#{f}"`.split[0]
 end
 
 0.upto(8) do |lod|
     object[lod] = Hash.new
-    ['earth', 'night', 'clouds'].each do |subdir|
+    LOD_DIRS.each do |subdir|
         Dir.entries("#{ARGV[0]}/#{subdir}").select { |e| e.start_with?("#{lod}-") }.each do |e|
             object[lod]["#{subdir}/#{e}"] = `md5sum -b "#{ARGV[0]}/#{subdir}/#{e}"`.split[0]
         end
