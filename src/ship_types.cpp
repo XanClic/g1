58269e391026bebcf154d815880442abee4cf848
Max Reitz <xanclic@gmail.com>
2015-10-22 00:42:17 +0200

    JSON structs: Simplify indexing
    * Describe the index files themselves, so they can be parsed more easily

--- src/ship_types.cpp
+++ src/ship_types.cpp
@@ -2,55 +2,33 @@
 #include <string>
 #include <unordered_map>
 
-#include "generic-data.hpp"
-#include "json.hpp"
-#include "json-structs.hpp"
+#include "serializer.hpp"
 #include "ship_types.hpp"
 
 
 std::unordered_map<std::string, const Ship *> ship_types;
 
 
+static void do_load_ship_types(void)
+{
+    ShipIndex si;
+    parse_file(&si, "config/ships/index.json");
+
+    for (const std::string &name: si.types) {
+        ship_types[name] = parse_file<Ship>("config/ships/" + name + ".json");
+    }
+}
+
+
 void load_ship_types(void)
 {
-    GDData *ship_index = json_parse_file("config/ships/index.json");
     try {
-        if (ship_index->type != GDData::ARRAY) {
-            throw std::runtime_error("config/ships/index.json must contain an "
-                                     "array of strings");
-        }
-
-        const GDArray &ship_array = *ship_index;
-        for (const GDData &ship: ship_array) {
-            if (ship.type != GDData::STRING) {
-                throw std::runtime_error("config/ships/index.json must contain "
-                                         "an array of strings");
-            }
-
-            const std::string &name = ship;
-            GDData *ship_spec = json_parse_file("config/ships/" + name
-                                                + ".json");
-            Ship *s = new Ship;
-            try {
-                parse(s, ship_spec);
-            } catch (...) {
-                delete s;
-                delete ship_spec;
-                throw;
-            }
-
-            delete ship_spec;
-
-            ship_types[name] = s;
-        }
+        do_load_ship_types();
     } catch (...) {
         for (auto &p: ship_types) {
             delete p.second;
         }
         ship_types.clear();
-        delete ship_index;
         throw;
     }
-
-    delete ship_index;
 }
