e2f860e4d37c2171f1643f07abfb35cada6ef29a
Hanna Reitz <xanclic@xanclic.moe>
2015-10-22 00:42:17 +0200

    JSON structs: Simplify indexing
    * Describe the index files themselves, so they can be parsed more easily

--- src/weapons.cpp
+++ src/weapons.cpp
@@ -1,57 +1,38 @@
 #include <vector>
 
-#include "generic-data.hpp"
-#include "json.hpp"
-#include "json-structs.hpp"
+#include "serializer.hpp"
 #include "weapons.hpp"
 
 
 std::vector<const WeaponClass *> weapon_classes;
 
 
+static void do_load_weapons(void)
+{
+    WeaponClassIndex wci;
+    parse_file(&wci, "config/weapons/index.json");
+
+    for (const std::string &type: wci.classes) {
+        WeaponClass *wc = parse_file<WeaponClass>("config/weapons/" + type +
+                                                  ".json");
+
+        if (weapon_classes.size() <= wc->type) {
+            weapon_classes.resize(wc->type + 1, nullptr);
+        }
+        weapon_classes[wc->type] = wc;
+    }
+}
+
+
 void load_weapons(void)
 {
-    GDData *weapon_type_index = json_parse_file("config/weapons/index.json");
     try {
-        if (weapon_type_index->type != GDData::ARRAY) {
-            throw std::runtime_error("config/weapons/index.json must contain "
-                                     "an array of strings");
-        }
-
-        const GDArray &weapon_type_array = *weapon_type_index;
-        for (const GDData &weapon_type: weapon_type_array) {
-            if (weapon_type.type != GDData::STRING) {
-                throw std::runtime_error("config/weapons/index.json must "
-                                         "contain an array of strings");
-            }
-
-            const std::string &type = weapon_type;
-            GDData *weapon_class_spec = json_parse_file("config/weapons/" + type
-                                                        + ".json");
-            WeaponClass *wc = new WeaponClass;
-            try {
-                parse(wc, weapon_class_spec);
-            } catch (...) {
-                delete wc;
-                delete weapon_class_spec;
-                throw;
-            }
-
-            delete weapon_class_spec;
-
-            if (weapon_classes.size() <= wc->type) {
-                weapon_classes.resize(wc->type + 1, nullptr);
-            }
-            weapon_classes[wc->type] = wc;
-        }
+        do_load_weapons();
     } catch (...) {
         for (const WeaponClass *wc: weapon_classes) {
             delete const_cast<WeaponClass *>(wc);
         }
         weapon_classes.clear();
-        delete weapon_type_index;
         throw;
     }
-
-    delete weapon_type_index;
 }
