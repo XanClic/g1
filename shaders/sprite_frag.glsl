907a00229f303de9af5d8564c9896b1a327cc23e
Max Reitz <xanclic@gmail.com>
2015-10-22 20:28:44 +0200

    HUD: Colored sprites
    * Use the sprites' own colors, and half the size of the delta velocity
      indicators

--- shaders/sprite_frag.glsl
+++ shaders/sprite_frag.glsl
@@ -6,10 +6,10 @@ in vec2 vf_pos;
 out vec4 out_col;
 
 uniform sampler2D sprite;
-uniform vec4 color;
+uniform float brightness;
 
 
 void main(void)
 {
-    out_col = texture(sprite, vf_pos) * color;
+    out_col = texture(sprite, vf_pos) * vec4(1.0, 1.0, 1.0, brightness);
 }
