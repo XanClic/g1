c5cee58ab73bb56509c6ef349d169ce7df381df4
Hanna Reitz <xanclic@xanclic.moe>
2015-11-13 01:52:57 +0100

    Scratch shader: Fix view direction calculation

--- shaders/scratch_vert.glsl
+++ shaders/scratch_vert.glsl
@@ -6,7 +6,7 @@ out vec2 vf_pos;
 out vec3 vf_dir;
 
 uniform vec3 cam_fwd, cam_right, cam_up;
-uniform float xhfov, yhfov;
+uniform float tan_xhfov, tan_yhfov;
 
 
 void main(void)
@@ -14,5 +14,5 @@ void main(void)
     gl_Position = vec4(va_pos, 0.0, 1.0);
     vf_pos = (va_pos + vec2(1.0, 1.0)) / 2.0;
 
-    vf_dir = cam_fwd + tan(va_pos.x * xhfov) * cam_right + tan(va_pos.y * yhfov) * cam_up;
+    vf_dir = cam_fwd + va_pos.x * tan_xhfov * cam_right + va_pos.y * tan_yhfov * cam_up;
 }
